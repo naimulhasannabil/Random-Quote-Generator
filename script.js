a51f3dc4e068ecf47479572b14b31e9b424e851d
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-11-15 11:06:48 +0600

    Update Project Code

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const randomQuote = [
     'Life is 10% what happens to you 90% how you react to it.',
     'Ever tired. Ever failed. No matter. Try Again. Fail again. Fail better.',
     'With the new day comes new strength and new thoughts.',
+    'If you are going to hell, Keep going.',
 ];
 
 const quoteElement = document.querySelector('#quote');
 const authorElement = document.querySelector('#author');
 const button = document.querySelector('#button');
+
+
+button.addEventListener('click', function(){
+   const quoteIndex = Math.floor(Math.random() * 5);
+//    console.log(quoteIndex);
+   quoteElement.textContent = randomQuote[quoteIndex];
+});
