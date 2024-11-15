0e94b9ad25363f16b2a0ddb2256c689574f522d0
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-11-15 10:44:44 +0600

    Update Random Quote Generator Code

---
+++ script.js
@@ -0,0 +1,10 @@
+const randomQuote = [
+    'Believe in yourself! Have a faith in your abilities! Without a humble but reasonable confident in your own powers you cannot be successful or happy.',
+    'Life is 10% what happens to you 90% how you react to it.',
+    'Ever tired. Ever failed. No matter. Try Again. Fail again. Fail better.',
+    'With the new day comes new strength and new thoughts.',
+];
+
+const quoteElement = document.querySelector('#quote');
+const authorElement = document.querySelector('#author');
+const button = document.querySelector('#button');
